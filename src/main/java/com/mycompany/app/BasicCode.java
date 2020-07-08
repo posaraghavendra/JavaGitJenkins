7aa13df6b7ac4983e53813fe07ae0c167f3e6f67
root <root@ip-172-31-19-244.us-east-2.compute.internal>
2020-07-08 12:26:46 +0000

    modify BasicCode file

--- src/main/java/com/mycompany/app/BasicCode.java
+++ src/main/java/com/mycompany/app/BasicCode.java
@@ -1,5 +1,5 @@
 package com.mycompany.app;
-
+   
 /**
  * Hello world!
  *
@@ -9,7 +9,7 @@ class BasicCode
     public static void main( String[] args )
     {
         System.out.println( "Basic Code!" );
-	System.out.println( "update for poll scm build trigger");
+	System.out.println( "update for poll scm build -------trigger");
     }
 }
 
