251f1bf6f6493cffa28b69c809080900e1a46885
posaraghavendra <posaraghavendra@gmail.com>
2020-07-08 20:06:43 +0530

    update code

--- src/main/java/com/mycompany/app/BasicCode.java
+++ src/main/java/com/mycompany/app/BasicCode.java
@@ -8,7 +8,7 @@ class BasicCode
 {
     public static void main( String[] args )
     {
-        System.out.println( "Basic Code!" );
+        System.out.println( "Basic Code!........" );
 	System.out.println( "update for poll scm build -------trigger");
     }
 }
